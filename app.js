a52e558dfb62f9b6421fa325508aa7c541ced117
sinduap <sindu.andita@gmail.com>
2019-10-23 15:50:04 +0700

    Route and form new campground

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-
 var publicDir = require("path").join(__dirname, "/public");
+
 app.use(express.static(publicDir));
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+var campgrounds = [
+    {
+        name: "Ranu Kumbolo",
+        image: "/img/RanuKumbolo.jpg"
+    },
+    {
+        name: "Panderman",
+        image: "/img/Panderman.jpg"
+    },
+    {
+        name: "Coban Talun",
+        image: "/img/CobanTalun.jpeg"
+    }
+]
 
 app.get("/", function (req, res) {
     res.render("landing");
 })
 
 app.get("/campgrounds", function (req, res) {
-    var campgrounds = [
-        {
-            name: "Ranu Kumbolo",
-            image: "/img/RanuKumbolo.jpg"
-        },
-        {
-            name: "Panderman",
-            image: "/img/Panderman.jpg"
-        },
-        {
-            name: "Coban Talun",
-            image: "/img/CobanTalun.jpeg"
-        }
-    ]
     res.render("campgrounds", {campgrounds: campgrounds});
 })
 
 app.post("/campgrounds", function(req, res) {
-    res.send("You've start");
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    //redirect back to campgrounds page
+    res.redirect("/campgrounds");
 })
 
 app.get("/campgrounds/new", function(req, res) {
